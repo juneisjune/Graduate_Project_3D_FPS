27cf689fe55820737b125d67e42053c0601e5955
juneisjune <yhs1535@naver.com>
2024-03-27 22:09:04 +0900

    졸업 작품

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnClickNewGame()
+    {
+        Debug.Log("Game start");
+        SceneManager.LoadScene("Second Stage");
+
+    }
+    public void OnClickTrain()
+    {
+        Debug.Log("Traing");
+        SceneManager.LoadScene("Training Center");
+
+
+    }
+    public void OnClickMain()
+    {
+        Debug.Log("Main");
+        SceneManager.LoadScene("Main");
+
+
+    }
+
+    public void OnClickQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying=false;
+#else
+        Application.Quit();
+#endif
+    }
+}
